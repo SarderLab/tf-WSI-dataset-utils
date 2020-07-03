ccfcb4f3cb875414db8dbfb8a542f55963183c34
Brendon Lutnick <brendonl@buffalo.edu>
2020-07-03 13:58:37 -0400

    Add files via upload

--- dataset_util.py
+++ dataset_util.py
@@ -1,4 +1,4 @@
-import os, cv2
+import os, cv2, random
 import numpy as np
 import scipy
 import scipy.misc
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from skimage.transform import PiecewiseAffineTransform, warp
 from skimage.color import rgb2hsv,hsv2rgb,rgb2lab,lab2rgb
 from skimage import exposure
+from xml_to_mask import xml_to_mask
 
 def save_wsi_thumbnail_mask(filename):
     '''
@@ -41,11 +42,12 @@ def save_wsi_thumbnail_mask(filename):
         mask_PIL = Image.fromarray(mask)
         mask_PIL.save(mask_path)
 
-def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=False):
+def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=0):
     '''
     takes a wsi and returns a random patch of patch_size
     downsample must be a multiple of 2
     run save_wsi_thumbnail_mask() before using this function
+    augment = [0,1] the percent of data that will be augmented
 
     '''
     try: augment = augment.numpy()
@@ -57,7 +59,7 @@ def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=False):
     try: downsample = downsample.numpy()
     except: downsample = downsample
 
-    if augment:
+    if augment > 0:
         # pad for affine
         patch_size = patch_size+4
 
@@ -66,96 +68,103 @@ def get_random_wsi_patch(filename, patch_size=256, downsample=4, augment=False):
     l_dims = wsi.level_dimensions
     level = wsi.get_best_level_for_downsample(downsample + 0.1)
 
-    mask_path = '{}_MASK.png'.format(filename.decode().split('.')[0])
+    # get or create wsi mask
+    try:
+        mask_path = '{}_MASK.png'.format(filename.decode().split('.')[0])
+    except:
+        mask_path = '{}_MASK.png'.format(filename.split('.')[0])
+    if not os.path.isfile(mask_path):
+        save_wsi_thumbnail_mask(filename)
     mask = np.array(Image.open(mask_path))
 
     def get_random_patch(wsi, l_dims, level, mask, patch_size, filename, downsample, augment):
-        iter = 0
+
+        # track locations and vectorize mask
+        [y_ind, x_ind] = np.indices(np.shape(mask))
+        y_ind = y_ind.ravel()
+        x_ind = x_ind.ravel()
+        mask_vec = mask.ravel()
+
         while True:
 
-            if iter > 200: # use next level if stuck in loop
+            if level == -1: # if no resolution works return white region
+                print('{} broken | using white patch...'.format(filename))
+                return np.ones((patch_size, patch_size,3))*255
+
+            try:
+                level_dims = l_dims[level]
+                level_downsample = wsi.level_downsamples[level]
+                thumbnail_size = float(max(mask.shape))
+                mask_scale = thumbnail_size/max(level_dims)
+                scale_factor = int(round(downsample / level_downsample))
+            except:
+                print('{} broken | using white patch...'.format(filename))
+                return np.ones((patch_size, patch_size,3))*255
+
+            try:
+                # select random pixel with tissue
+                idx = random.choice(np.argwhere(mask_vec==255))[0]
+                x_mask = x_ind[idx]
+                y_mask = y_ind[idx]
+
+                # calc wsi patch start indicies
+                patch_width = patch_size*scale_factor
+                x_start = int( (x_mask / mask_scale) - (patch_width/2))
+                y_start = int( (y_mask / mask_scale) - (patch_width/2))
+
+                region = wsi.read_region((int(x_start*level_downsample),int(y_start*level_downsample)), level, (patch_width,patch_width))
+
+                if scale_factor > 1:
+                    region = region.resize((patch_size, patch_size), resample=1)
+                region = np.array(region)[:,:,:3]
+
+                def colorshift(img, hbound=0.025, lbound=0.015): #Shift Hue of HSV space and Lightness of LAB space
+                    hShift=np.random.normal(0,hbound)
+                    lShift=np.random.normal(1,lbound)
+                    img=rgb2hsv(img)
+                    img[:,:,0]=(img[:,:,0]+hShift)
+                    img=hsv2rgb(img)
+                    img=rgb2lab(img)
+                    img[:,:,0]=exposure.adjust_gamma(img[:,:,0],lShift)
+                    img=lab2rgb(img)
+                    return img
+
+                def PiecewiseAffine(img, points=8):
+                    ### piecwise affine ###
+                    rows, cols = img.shape[0], img.shape[1]
+                    src_cols = np.linspace(0, cols, points)
+                    src_rows = np.linspace(0, rows, points)
+                    src_rows, src_cols = np.meshgrid(src_rows, src_cols)
+                    src = np.dstack([src_cols.flat, src_rows.flat])[0]
+                    # add offset
+                    dst_rows = np.zeros(src[:, 1].shape) + src[:, 1]
+                    for i in list(range(points))[1:-1]:
+                        dst_rows[i::points] += np.random.normal(loc=0, scale=rows/(points*10), size=dst_rows[i::points].shape)
+                    dst_cols = np.zeros(src[:, 0].shape) + src[:, 0]
+                    dst_cols[points:-points] += np.random.normal(loc=0,scale=rows/(points*10), size=dst_cols[points:-points].shape)
+                    dst = np.vstack([dst_cols, dst_rows]).T
+                    # compute transform
+                    tform = PiecewiseAffineTransform()
+                    tform.estimate(src, dst)
+                    # apply transform
+                    img = warp(img, tform, output_shape=(rows, cols))
+                    return img
+
+                if np.random.random() < augment:
+                    # augment image
+                    region = (region/255.).astype(np.float64)
+                    region = colorshift(region)
+                    region = PiecewiseAffine(region)
+                    region = np.uint8(region*255.)
+                    # unpad
+                    region = region[2:-2,2:-2,:]
+
+                return region
+
+            except:
                 print('{} broken for level {}'.format(filename,level))
                 level -= 1
                 print('\ttrying level {}'.format(level))
-                if level == -1: # if no resolution works return white region
-                    print('{} broken | using white patch...'.format(filename))
-                    return np.ones((patch_size, patch_size,3))*255
-                iter = 0
-
-            if iter == 0: # calculate dimensions and scales
-                try:
-                    level_dims = l_dims[level]
-                    level_downsample = wsi.level_downsamples[level]
-                    thumbnail_size = float(max(mask.shape))
-                    mask_scale = thumbnail_size/max(level_dims)
-                    scale_factor = int(round(downsample / level_downsample))
-                except:
-                    print('{} broken | using white patch...'.format(filename))
-                    return np.ones((patch_size, patch_size,3))*255
-
-            iter += 1
-            x_start = int(np.random.uniform(low=0, high=level_dims[0]-patch_size*scale_factor))
-            y_start = int(np.random.uniform(low=0, high=level_dims[1]-patch_size*scale_factor))
-
-            mask_x_start = int(x_start*mask_scale)
-            mask_x_stop = int((x_start + patch_size*scale_factor)*mask_scale)
-            mask_y_start = int(y_start*mask_scale)
-            mask_y_stop = int((y_start + patch_size*scale_factor)*mask_scale)
-
-            if np.sum(mask[mask_y_start:mask_y_stop, mask_x_start:mask_x_stop]) > (mask_y_stop-mask_y_start) * (mask_x_stop-mask_x_start) * 255 / 3:
-
-                try:
-                    region = wsi.read_region((int(x_start*level_downsample),int(y_start*level_downsample)), level, (patch_size*scale_factor,patch_size*scale_factor))
-
-                    if scale_factor > 1:
-                        region = region.resize((patch_size, patch_size), resample=1)
-                    region = np.array(region)[:,:,:3]
-
-                    def colorshift(img, hbound=0.025, lbound=0.015): #Shift Hue of HSV space and Lightness of LAB space
-                        hShift=np.random.normal(0,hbound)
-                        lShift=np.random.normal(1,lbound)
-                        img=rgb2hsv(img)
-                        img[:,:,0]=(img[:,:,0]+hShift)
-                        img=hsv2rgb(img)
-                        img=rgb2lab(img)
-                        img[:,:,0]=exposure.adjust_gamma(img[:,:,0],lShift)
-                        img=lab2rgb(img)
-                        return img
-
-                    def PiecewiseAffine(img, points=8):
-                        ### piecwise affine ###
-                        rows, cols = img.shape[0], img.shape[1]
-                        src_cols = np.linspace(0, cols, points)
-                        src_rows = np.linspace(0, rows, points)
-                        src_rows, src_cols = np.meshgrid(src_rows, src_cols)
-                        src = np.dstack([src_cols.flat, src_rows.flat])[0]
-                        # add offset
-                        dst_rows = np.zeros(src[:, 1].shape) + src[:, 1]
-                        for i in list(range(points))[1:-1]:
-                            dst_rows[i::points] += np.random.normal(loc=0, scale=rows/(points*10), size=dst_rows[i::points].shape)
-                        dst_cols = np.zeros(src[:, 0].shape) + src[:, 0]
-                        dst_cols[points:-points] += np.random.normal(loc=0,scale=rows/(points*10), size=dst_cols[points:-points].shape)
-                        dst = np.vstack([dst_cols, dst_rows]).T
-                        # compute transform
-                        tform = PiecewiseAffineTransform()
-                        tform.estimate(src, dst)
-                        # apply transform
-                        img = warp(img, tform, output_shape=(rows, cols))
-                        return img
-
-                    if augment:
-                        if np.random.random() > .5:
-                            # augment image
-                            region = (region/255.).astype(np.float64)
-                            region = colorshift(region)
-                            region, patch_mask = PiecewiseAffine(region)
-                            region = (region*255.)
-                        # unpad
-                        region = region[2:-2,2:-2,:]
-
-                    return region
-
-                except: pass
 
     region = get_random_patch(wsi, l_dims, level, mask, patch_size, filename, downsample, augment)
     # region = np.transpose(region, (2,0,1)) # [CWH]
--- example_usage.py
+++ example_usage.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from dataset_util import save_wsi_thumbnail_mask, get_random_wsi_patch
+from dataset_util import get_random_wsi_patch
 
 def input_setup(batch_size=32, dataset_dir='./path_to_folder_with_WSIs', wsi_ext='.svs'):
 
@@ -14,10 +14,6 @@ def input_setup(batch_size=32, dataset_dir='./path_to_folder_with_WSIs', wsi_ext
     wsi_paths = list(glob('{}*{}'.format(dataset_dir, wsi_ext)))
     wsi_paths = [str(path) for path in wsi_paths]
 
-    # create WSI thumbnail masks
-    for wsi_path in wsi_paths:
-        save_wsi_thumbnail_mask(wsi_path)
-
     random.shuffle(wsi_paths)
     image_count = len(wsi_paths)
     print('found: {} images from WSI dataset'.format(image_count))
@@ -25,7 +21,7 @@ def input_setup(batch_size=32, dataset_dir='./path_to_folder_with_WSIs', wsi_ext
     # setup tf dataset using py_function
     path_ds = tf.data.Dataset.from_tensor_slices(wsi_paths)
     path_ds = path_ds.repeat(100)
-    wsi_dataset = path_ds.map(lambda filename: tf.py_function(get_random_wsi_region, [filename], tf.float32), num_parallel_calls=40)
+    wsi_dataset = path_ds.map(lambda filename: tf.py_function(get_random_wsi_patch, [filename], tf.float32), num_parallel_calls=40)
     wsi_dataset = wsi_dataset.shuffle(100)
     wsi_dataset = wsi_dataset.batch(batch_size=batch_size, drop_remainder=True)
     wsi_dataset = wsi_dataset.prefetch(buffer_size=1) # <-- very important for efficency
